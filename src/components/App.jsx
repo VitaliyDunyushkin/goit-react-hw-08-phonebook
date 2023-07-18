572b9cef5995cc8e176fdc665a7fb7dd4c40deb4
VitaliyDunyushkin <117518206+VitaliyDunyushkin@users.noreply.github.com>
2023-07-18 18:36:20 +0300

    toooo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
+// import ContactForm from './ContactForm';
+// import ContactList from './ContactList';
+// import Filter from './Filter';
 import UserMenu from './UseMenu';
 
 import RegisterPage from 'pages/RegisterPage';
 
-import { fetchContacts } from 'redux/operations';
-import { selectError, selectIsLoading } from 'redux/selectors';
+// import { fetchContacts } from 'redux/operations';
+// import { selectError, selectIsLoading } from 'redux/selectors';
 
 export function App() {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
 
   // useEffect(() => {
   //   dispatch(fetchContacts());
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,16 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 import {
   persistStore,
   persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
+  // FLUSH,
+  // REHYDRATE,
+  // PAUSE,
+  // PERSIST,
+  // PURGE,
+  // REGISTER,
 } from 'redux-persist';
+
 import storage from 'redux-persist/lib/storage';
 
 import { contactsReducer } from './contactsSlice';
