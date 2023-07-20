2e789c49e38ef603989ddbf045f539118d464e91
VitaliyDunyushkin <117518206+VitaliyDunyushkin@users.noreply.github.com>
2023-07-20 15:40:39 +0000

    Deploying to gh-pages from @ VitaliyDunyushkin/goit-react-hw-08-phonebook@45a77d8cf23e5b234e3664fd6ccc5d990dd8d514 🚀

---
+++ static/js/206.fdbf672c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{206:function(e,t,n){n.r(t);n(791);var i=n(329),l={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:l.container,children:(0,i.jsx)("h1",{style:l.title,children:"Hello, have a nice day!"})})}}}]);
+//# sourceMappingURL=206.fdbf672c.chunk.js.map
